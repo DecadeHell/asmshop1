d5412f07e0585bb51376f2c485b20aaa62c1b474
datnhism <98210435+datnhism@users.noreply.github.com>
2023-04-14 16:28:59 +0700

    password configure

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //mongodb+srv://truongphatdat2002ct:<password>@cluster0.igwt55s.mongodb.net/test
 
-const url = "mongodb+srv://truongphatdat2002ct:phatdat0202@cluster0.igwt55s.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://truongphatdat2002ct:OrJUcc82Fpu2loDY@cluster0.igwt55s.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
