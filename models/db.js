4949c68548cc8d8895fe437b044f466e4252bc3f
DecadeHell <gjsdsdfsda@gmail.com>
2023-04-21 12:18:44 +0700

    make it betteraaaa

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //mongodb+srv://truongphatdat2002ct:<password>@cluster0.igwt55s.mongodb.net/test
 
-const url = "mongodb+srv://truongphatdat2002ct:OrJUcc82Fpu2loDY@cluster0.igwt55s.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://gjsdsdfsda:Sb2ocYmbHi2uV0mT@cluster0.civgvpv.mongodb.net/test"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
